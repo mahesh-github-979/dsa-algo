14ba80f1f0d8563593af3c54f6b22a2f0222e6d1
v_mahesh.mandala@jsw.in <mandalamahesh>
2022-08-22 17:50:44 +0530

    initial commit

---
+++ dsa-algo/src/main/java/com/code/dsaalgo/DsaAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.code.dsaalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaAlgoApplication.class, args);
+	}
+
+}
---
+++ dsa-algo/src/test/java/com/code/dsaalgo/DsaAlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.dsaalgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaAlgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
